9d2365dbb7b1648a5a1ae5d460c2f4f8d30c01a2
gaspan <gaspan619@gmail.com>
2017-03-11 23:19:51 +0700

    git push tugas 1

---
+++ pertemuan-1/Hitungluasbelahketupat.py
@@ -0,0 +1,30 @@
+print('Hitung luas belah ketupat')
+d1 = input('diagonal 1 = ' )
+d2 = input('diagonal 2 = ' )
+L = (d1 * d2) / 2
+Luas = str(L)
+if Luas == '1':
+	x = 'satu'
+elif Luas == '2':
+	x = 'dua'	
+elif Luas == '3':
+	x = 'tiga'
+elif Luas == '4':
+	x = 'empat'
+elif Luas == '5':
+	x = 'lima'
+elif Luas == '6':
+	x = 'enam'
+elif Luas == '7':
+	x = 'tujuh'
+elif Luas == '8':
+	x = 'delapan'
+elif Luas == '9':
+	x = 'sembilan'
+elif Luas == '0':
+	x = 'nol'
+else:
+	x = 'lebih dari sembilan'
+
+print('Hasil luas belah ketupat = ' + Luas + '(' + x + ')')
+
