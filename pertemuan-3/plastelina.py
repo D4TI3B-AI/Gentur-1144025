11569b5527205bcf673b695bd09c603b3200bfe3
gaspan <gaspan619@gmail.com>
2017-03-27 16:16:52 +0700

    alhamdulillah... udah

---
+++ pertemuan-3/plastelina.py
@@ -0,0 +1,23 @@
+print("game plastelina")
+print("pilih domba/sayur/srigala")
+a = raw_input('pilih ?')
+if a == "domba":
+    b = raw_input('nice, pilih? ')
+    if b == "sayur":
+        c = raw_input('nice, pilih? ')
+        if c == "domba":
+            d = raw_input('nice, pilih? ')
+            if d == "srigala":
+                e = raw_input('nice, pilih? ')
+                if e == "domba":
+                    print("nice, you win")
+                else:
+                    print('noob')
+            else:
+                print('noob')
+        else:
+            print('noob')
+    else:
+        print('noob')
+else:
+    print('noob')
